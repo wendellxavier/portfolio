3bb400dab0946dfa273717c881aae89a20e68d70
wendellxavier <wendell_xw20@hotmail.com>
2024-06-27 14:15:13 -0300

    first portfolio

---
+++ static/script.js
@@ -0,0 +1,59 @@
+const menuMobile = document.querySelector('.menu-mobile')
+const body = document.querySelector('body')
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains("bi-list") ? menuMobile.classList.replace("bi-list","bi-x") : menuMobile.classList.replace("bi-x", "bi-list")
+    
+    body.classList.toggle("menu-nav-active")
+
+    
+});
+
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
+
+
+
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+    
+    item.forEach(element => {
+        if (windowTop > element.offsetTop){
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll()
+
+window.addEventListener("scroll", () => {
+    animeScroll()
+})
+
+
+const btnEnviar = document.querySelectorAll('btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEnventListener("click", () => {
+    btnEnviarLoader.computedStyleMap.display = "block";
+    btnEnviar.style.display= "none";
+
+})
+
+setTimeout(() => {
+    document.querySelector('#alerta').style.display = 'none';
+}, 5000)
